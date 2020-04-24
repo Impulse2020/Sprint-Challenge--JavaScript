fdddf44b1290804adc82e246bb5df52445a69037
Impulse2020 <irishht@gmail.com>
2020-04-24 15:15:12 -0400

    working cuboids

--- challenges/prototypes.js
+++ challenges/prototypes.js
@@ -20,7 +20,7 @@ class CuboidMaker{
   Formula for cuboid volume: length * width * height
 */
 CuboidMaker.prototype.volume =function(){ 
-  return CuboidMaker.length * CuboidMaker.width * CuboidMaker.height
+  return this.length * this.width * this.height
 };
 
 
@@ -30,7 +30,7 @@ CuboidMaker.prototype.volume =function(){
   Formula for cuboid surface area of a cube: 2 * (length * width + length * height + width * height)
 */
 CuboidMaker.prototype.surfaceArea = function(){
-  return 2 * (length * width + length * height + width * height)};
+  return 2 * (this.length * this.width + this.length * this.height + this.width *this.height)};
 
 /* == Step 4: Create a new object that uses CuboidMaker ==
   Create a cuboid object that uses the new keyword to use our CuboidMaker constructor
