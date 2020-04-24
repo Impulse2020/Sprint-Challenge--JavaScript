d726fe8d0477df02249f71ac4820dcd70b7e4490
Impulse2020 <irishht@gmail.com>
2020-04-24 15:49:15 -0400

    Completes objects-arrays.js

--- challenges/classes.js
+++ challenges/classes.js
@@ -1,5 +1,5 @@
 // 1. Copy and paste your prototype in here and refactor into class syntax.
-class CuboidMaker{
+class CuboidMaker2{
     constructor(length,width, height){
       this.length = length;
       this.width= width;
@@ -12,7 +12,7 @@ class CuboidMaker{
        return 2 * (this.length * this.width + this.length * this.height + this.width *this.height);
     }
   }
-  let cuboid = new CuboidMaker(4 ,5, 5);
+  let cuboid = new CuboidMaker2(4 ,5, 5);
 // Test your volume and surfaceArea methods by uncommenting the logs below:
  console.log(cuboid.volume()); // 100
  console.log(cuboid);
--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -126,9 +126,7 @@ The zoos need a list of all their animal's names (animal_name only) converted to
 
 */
 
-const lowCaseAnimalNames = zooAnimals.map(element => {
-    name : element.animal_name.toLowerCase();
-})
+const lowCaseAnimalNames = zooAnimals.map(element => element.animal_name.toLowerCase());
 
 console.log(lowCaseAnimalNames);
 
@@ -137,7 +135,7 @@ console.log(lowCaseAnimalNames);
 The zoos are concerned about animals with a lower population count. Using filter, create a new array of objects called lowPopulationAnimals which contains only the animals with a population less than 5.
 
 */
-const lowPopulationAnimals = [];
+const lowPopulationAnimals = zooAnimals.filter(element => element.population <=5);
 console.log(lowPopulationAnimals);
 
 /* Request 4: .reduce() 
@@ -145,7 +143,7 @@ console.log(lowPopulationAnimals);
 The zoos need to know their total animal population across the United States. Find the total population from all the zoos using the .reduce() method. Remember the reduce method takes two arguments: a callback (which itself takes two args), and an initial value for the count.
 
 */
-const populationTotal = 0;
+const populationTotal = zooAnimals.reduce((animals, zoos ) =>  zoos.population + animals, 0);
 console.log(populationTotal);
 
 
